803d080c7d0a1ec8a90846636b6b3a2caa2aa75c
BrandonArce27 <brandonarce2712@gmail.com>
2023-12-14 23:24:43 -0600

    last changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ user, handleLogout }) => {
       </p>
       <div className="flex-grow space-x-4">
         <button
-          onClick={() => navigate("")}
+          onClick={() => navigate("/")}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 transform hover:scale-105"
         >
           Home
@@ -27,7 +27,7 @@ const Header: React.FC<HeaderProps> = ({ user, handleLogout }) => {
           Trending movies
         </button>
         <button
-          onClick={() => navigate("/favorites")}
+          onClick={() => navigate("/")}
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 transform hover:scale-105"
         >
           Favorites
